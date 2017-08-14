e13447647c01d950ba9297e8d0e6746d286404cb
BOBO <yuanboi88@163.com>
2017-08-14 09:31:19 +0800

    top style

--- src/components/common/Top.vue
+++ src/components/common/Top.vue
@@ -26,7 +26,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
   .top{ position: absolute; z-index: 2; top: 0; padding-left: 260px; width: 100%; height: 80px; background: #11172C; box-shadow: 0 4px 10px 0 rgba(0,0,0,0.10);
-    .user-name{ padding: 15px 20px; height: 80px; line-height: 50px; vertical-align: middle;
+    .user-name{ padding: 15px 20px; height: 80px; line-height: 50px; vertical-align: middle; font-weight: normal; font-style: normal;
       img{ display: inline-block; width: 50px; height: 50px; border-radius: 50px; vertical-align: middle;}
       span{ margin-left: 10px; display: inline-block; height: 50px; line-height: 50px; vertical-align: middle;}
     }
