b4803ebff16e478806c44ec78c93ad1639b01d6e
BOBO <yuanboi88@163.com>
2017-08-29 09:56:39 +0800

    配置文件修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://172.25.50.17:8080"'
+  BASE_URL: '"http://119.23.35.234:8088"'
 })
